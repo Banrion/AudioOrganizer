98baa410eed0c3a8fb01d6252cea50071898ee4c
Chris <chriswc14@yahoo.com>
2017-09-29 15:09:54 -0500

    Created Java project and added simple program.

---
+++ AudioOrganizer/src/organizer/Orgaizer.java
@@ -0,0 +1,11 @@
+package organizer;
+
+public class Orgaizer {
+	public Orgaizer() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static void Greeting() {
+		System.out.println("Hi, from Orgaizer.");
+	}
+}
